34114e138afb6e62679104a1562c7ace3e704386
NKRUS <nik1rus2moscow3@gmail.com>
2018-10-18 20:29:15 +0300

    Добавлено задание по второму занятию и заготовки для начала решения

---
+++ src/ru/savdan/tutorial/lesson2/task2/PrimeNumbers.java
@@ -0,0 +1,10 @@
+package ru.savdan.tutorial.lesson2.task2;
+
+/**
+ * Тут вторая задачка про простые числа, остальные package-ы и классы сам создашь для оставшихся задач
+ */
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+    }
+}

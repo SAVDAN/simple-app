8653d57cdaaae3578a3924679be22bebe0c9d2fa
SAVDAN <savdan2304@mail.ru>
2018-11-10 20:04:30 +0300

    Урок 2: Среднее арифметическое 100%(есть вопрос)

--- src/ru/savdan/tutorial/lesson2/task1/CyclicArray.java
+++ src/ru/savdan/tutorial/lesson2/task1/CyclicArray.java
@@ -1,15 +1,29 @@
 package ru.savdan.tutorial.lesson2.task1;
 
-/**
- * Создаешь классы чтобы имена соответствовали смыслу решаемой задачи.
- * Класс всегда должен быть существительным в английском языке
- */
+import java.util.Scanner;
+
 public class CyclicArray {
 
-    /**
-     * В каждом классе будет метод main( Да он может быть в нескольких классах, несколько разных запускалок)
-     */
     public static void main(String[] args) {
+
+          System.out.println("Введите число, а я передвину массив: ");
+          Scanner in = new Scanner(System.in);
+          int shift = in.nextInt();
+
+
+
+        int array[] = new int[5];
+
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+
+        for (int i = 0; i < array.length + shift >> 1; i++) {
+            System.out.println(array[i]);
+        }
 
     }
 }
+
---
+++ src/ru/savdan/tutorial/lesson2/task3/arrayminmax.java
@@ -0,0 +1,37 @@
+package ru.savdan.tutorial.lesson2.task3;
+
+
+public class arrayminmax {
+    public static void main(String[] args) {
+
+        int array[] = new int[6];
+
+        int min;
+        int max;
+        int sum = 0;   // Тут мне айдиа предложила прировнять к нулю и тогда заработало. Тут не понял.
+
+        array[0] = 2;
+        array[1] = 4;
+        array[2] = 5;
+        array[3] = 6;
+        array[4] = 7;
+        array[5] = 100;
+
+        min = max = array[0];
+
+        for (int i = 0; i < 6 ; i++) {
+            if(array[i] < min) min = array[i];
+            if(array[i] > max) min = array[i];
+
+            sum = (min + max) / 2;
+
+        }
+
+
+
+        System.out.println(" Среднее арифметическое " + sum);
+
+
+
+        }
+    }
